8ca155d51cadfb58b1a8b04d213d0801548d1de5
Concepción <Concepcin@LC05H31005>
2017-12-22 13:01:35 -0300

    Agregando System.out

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -15,7 +15,11 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        System.out.println("Feliz Navidad");
+        
+        
+        
     }
     
 }
